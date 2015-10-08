a7ebbda71816ab8d6728198162754943fbd37229
YoshinakaHayato <friendly@YoshinakaHayato-no-MacBook-Pro.local>
2015-10-08 12:11:59 +0900

    no message

---
+++ src/js/sample.js
@@ -0,0 +1,119 @@
+var atable = {
+	"table": "Aテーブル",
+	"columns": [
+		{
+			"name": "a_id",
+			"dataType": "int",
+			"leng": "4",
+			"const": [
+				"no null"
+			]
+		},
+		{
+			"name": "b",
+			"dataType": "string",
+			"leng": "16",
+			"const": [
+				"no null"
+			]
+		},
+		{
+			"name": "c",
+			"dataType": "string",
+			"leng": "16",
+			"const": [
+				"no null"
+			]
+		},
+
+	],
+	"constraint": [
+			{
+					"primary_key": [
+							"a_id"
+							],
+					"foreign_key": [
+						{
+								"col_name":"b",
+								"table":"bテーブル",
+								"parent_col":"b"
+						}
+					]
+			}
+	],
+	"data": [
+		{
+			"a_id":"0001",
+			"b":"ああああ",
+			"c":"ううううう"
+		},
+		{
+			"a_id":"0002",
+			"b":"いいいい",
+			"c":"えええええ"
+		}
+	]
+
+	};
+	
+var btable = {
+	"table": "Bテーブル",
+	"columns": [
+		{
+			"name": "d_id",
+			"dataType": "int",
+			"leng": "4",
+			"const": [
+				"no null"
+			]
+		},
+		{
+			"name": "e",
+			"dataType": "string",
+			"leng": "16",
+			"const": [
+				"no null"
+			]
+		},
+
+	],
+	"constraint": [
+			{
+					"primary_key": [
+							"d_id"
+							],
+			}
+	],
+	"data": [
+		{
+			"d_id":"0003",
+			"e":"かかかか",
+		},
+		{
+			"d_id":"0004",
+			"e":"けけけけ",
+		}
+	]
+
+	};
+
+var aaa = JSON.stringify(atable);
+localStorage.setItem("atable",aaa);
+
+var bbb = JSON.stringify(btable);
+localStorage.setItem("btable",bbb);
+
+
+
+
+ 
+$(function(){
+
+    
+    // データの全削除
+    $('#clear').click(function() {
+        localStorage.clear();
+        showStorage();
+    });
+ 
+});
