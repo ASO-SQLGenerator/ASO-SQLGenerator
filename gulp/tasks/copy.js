f08cb001ba0f89648b07619b5be68c2daeeb6e17
Ryu Hanaoka <doradora56@gmail.com>
2015-10-20 05:27:15 +0900

    copyをhtmlとcssに分割
    jsファイルの名前をreplaceをするため

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp');
+var replace = require('gulp-replace');
 var config = require('../config').copy;
 
-gulp.task( 'copy', function() {
-  return gulp.src([config.html, config.css],
+gulp.task( 'copy:html', function() {
+  return gulp.src([config.html],
+    {base: config.src})
+    .pipe(replace('./bundle.js', './bundle.min.js'))
+    .pipe(gulp.dest(config.dest));
+});
+
+gulp.task( 'copy:css', function() {
+  return gulp.src([config.css],
     {base: config.src})
     .pipe(gulp.dest(config.dest));
 });
+
+gulp.task( 'copy', ['copy:html', 'copy:css']);
