5c314def06505c71f035437ff86cd91ea61a30be
Ryu Hanaoka <doradora56@gmail.com>
2015-10-20 09:37:24 +0900

    browser-syncの設定
    vagrant上からも見えるように設定

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var PORT = 3000;
+gulp.task('watch', function() {
+  browserSync({
+    port: PORT,
+    host: '192.168.33.20',
+    open: false,
+    server: {
+      baseDir: './src'
+    }
+  });
+  gulp.watch('./src/bundle.js', browserSync.reload);
+  gulp.watch('./src/**/*.html', browserSync.reload);
+});
+
+
+gulp.task('watch:lint', function() {
+  gulp.watch('src/js/*.js', ['lint']);
+});
