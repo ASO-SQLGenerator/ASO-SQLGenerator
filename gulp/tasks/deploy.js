7920624c1ca72e49c9ed612114b5fa764bf34ab5
Ryu Hanaoka <doradora56@gmail.com>
2015-10-20 15:28:43 +0900

    fix typo

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -7,6 +7,6 @@ var deploy = require('gulp-gh-pages');
 gulp.task('deploy', ['release'], function() {
   return gulp.src('./public/**/*')
     .pipe(deploy({
-      remoteUrl: 'https://' + process.env.ITHUB_ACCESS_TOKEN + '@github.com:ASO-SQLGenerator/ASO-SQLGenerator.git'
+      remoteUrl: 'https://' + process.env.GITHUB_ACCESS_TOKEN + '@github.com:ASO-SQLGenerator/ASO-SQLGenerator.git'
     }));
 });
