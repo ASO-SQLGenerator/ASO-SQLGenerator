412b2a44b492afc315a3b67d1a3492ad404488d3
Ryu Hanaoka <doradora56@gmail.com>
2015-10-20 19:34:45 +0900

    デプロイ方法変更

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+rm -rf public || exit 0;
+mkdir public
+
+npm run release
+
+cd public
+
+git init
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/ASO-SQLGenerator/ASO-SQLGenerator.github.io.git" master:gh-pages > /dev/null 2>&1
