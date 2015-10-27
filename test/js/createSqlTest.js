875c03f094de2a6aa421e6711cf38fcc47ae473a
Ryu Hanaoka <doradora56@gmail.com>
2015-10-27 12:01:01 +0900

    リファクタリング

--- test/js/createSqlTest.js
+++ test/js/createSqlTest.js
@@ -4,98 +4,97 @@ var assert = require('power-assert');
 var createSql = require('../../src/js/createSql.js');
 
 describe('createSqlで', function() {
-  var table;
-  var table2;
-  var table3;
-  var table4;
-  table = {
-    'table': 'Aテーブル',
-    'columns': [
-      {
-        'name': 'a_id',
-        'dataType': 'int',
-        'leng': '4',
-        'const': []
-      },
-      {
-        'name': 'b',
-        'dataType': 'string',
-        'leng': '16',
-        'const': [
-          'NOT NULL'
-        ]
-      },
-      {
-        'name': 'c',
-        'dataType': 'string',
-        'leng': '',
-        'const': []
-      }
-    ],
-    'constraint': {
-      'primary_key': [
-        'a_id',
-        'b'
-      ],
-      'foreign_key': [
-        {
-          'col_name': 'b',
-          'table': 'bテーブル',
-          'parent_col': 'b'
-        }
-      ]
-    }
-  };
-  table2 = {
-    'table': 'Aテーブル',
-    'columns': [
-      {
-        'name': 'a_id',
-        'dataType': 'int',
-        'leng': '4',
-        'const': []
-      }
-    ],
-    'constraint': {}
-  };
-  table3 = {
-    'table': 'Aテーブル',
-    'columns': [
-      {
-        'name': 'a_id',
-        'dataType': 'int',
-        'leng': '4',
-        'const': []
-      }
-    ],
-    'constraint': {
-      'primary_key': [
-        'a_id'
-      ]
-    }
-  };
-  table4 = {
-    'table': 'Aテーブル',
-    'columns': [
-      {
-        'name': 'a_id',
-        'dataType': 'int',
-        'leng': '4',
-        'const': []
-      }
-    ],
-    'constraint': {
-      'foreign_key': [
-        {
-          'col_name': 'b',
-          'table': 'bテーブル',
-          'parent_col': 'b'
-        }
-      ]
-    }
-  };
-
   describe('tableからcreate文を生成', function() {
+    var table;
+    var table2;
+    var table3;
+    var table4;
+    table = {
+      'table': 'Aテーブル',
+      'columns': [
+        {
+          'name': 'a_id',
+          'dataType': 'int',
+          'leng': '4',
+          'const': []
+        },
+        {
+          'name': 'b',
+          'dataType': 'string',
+          'leng': '16',
+          'const': [
+            'NOT NULL'
+          ]
+        },
+        {
+          'name': 'c',
+          'dataType': 'string',
+          'leng': '',
+          'const': []
+        }
+      ],
+      'constraint': {
+        'primary_key': [
+          'a_id',
+          'b'
+        ],
+        'foreign_key': [
+          {
+            'col_name': 'b',
+            'table': 'bテーブル',
+            'parent_col': 'b'
+          }
+        ]
+      }
+    };
+    table2 = {
+      'table': 'Aテーブル',
+      'columns': [
+        {
+          'name': 'a_id',
+          'dataType': 'int',
+          'leng': '4',
+          'const': []
+        }
+      ],
+      'constraint': {}
+    };
+    table3 = {
+      'table': 'Aテーブル',
+      'columns': [
+        {
+          'name': 'a_id',
+          'dataType': 'int',
+          'leng': '4',
+          'const': []
+        }
+      ],
+      'constraint': {
+        'primary_key': [
+          'a_id'
+        ]
+      }
+    };
+    table4 = {
+      'table': 'Aテーブル',
+      'columns': [
+        {
+          'name': 'a_id',
+          'dataType': 'int',
+          'leng': '4',
+          'const': []
+        }
+      ],
+      'constraint': {
+        'foreign_key': [
+          {
+            'col_name': 'b',
+            'table': 'bテーブル',
+            'parent_col': 'b'
+          }
+        ]
+      }
+    };
     it('create文のテーブルか表示できているか', function() {
       var actual = createSql.create(table);
       assert.equal(actual, 'CREATE TABLE Aテーブル (a_id int(4), b string(16) NOT NULL, c string) PRIMARY KEY(a_id, b) FOREIGN KEY (b) REFERENCES bテーブル(b)');
@@ -116,7 +115,11 @@ describe('createSqlで', function() {
 
   describe('INSERT文を生成', function() {
     var insertTable;
+    var except;
+    var actual;
     beforeEach(function(done) {
+      except = '';
+      actual = '';
       insertTable = {
         'table': 'Aテーブル',
         'data': [{
@@ -127,14 +130,12 @@ describe('createSqlで', function() {
       done();
     });
     it('INSERT文が表示できているか', function() {
-      var actual = createSql.insert(insertTable);
-      var except = 'INSERT INTO Aテーブル (id, name) VALUES (1, \'aaa\')';
+      actual = createSql.insert(insertTable);
+      except = 'INSERT INTO Aテーブル (id, name) VALUES (1, \'aaa\')';
       assert.equal(actual, except);
     });
 
     it('複数の値があるINSERT文が表示できているか', function() {
-      var except;
-      var actual;
       insertTable.data = [
         {
           'id': 1,
