3bdecb6f37dcebaf865f34b0ccabfed28969e4dc
hayashi <highen4.wtf@gmail.com>
2015-10-29 03:13:52 +0900

    どぉしてゅぅこときかないの。。

--- HaYaShI/js/select_draganddrop.js
+++ HaYaShI/js/select_draganddrop.js
@@ -1,32 +1,33 @@
-//	.dragがドラッグされている際の動き
 $(function() {
+
+var i = 0;
+
     $('.selecttable').draggable({
+    	connectToSortable : '.sdrop', //ドロップ先にクローン作成
         helper:'clone',
         revert : 'invalid',
     });
 
     $('.selectall').draggable({
+    	connectToSortable : '.sdrop',
         helper:'clone',
         revert : 'invalid',
     });
 
     $('.selectcondition').draggable({
+        connectToSortable : '.sdrop',
         helper:'clone',
         revert : 'invalid',
     });
-
+    
     $('.sdrop').droppable({
-        accept: '.zenbudame',
     });
 
-//});
 
-    //	.dropがドラッグされている際の動き
 
     $('.sdrop').draggable({
         connectToSortable : '.sspace',
         helper:'clone',
-        //helper: 'clone',
         helper : function() {
             return $(this).clone().addClass('dragsdrop');
         },
@@ -37,25 +38,23 @@ $(function() {
         accpet:'.sdrop',
         greedy: true,
         drop: function( event, ui ) {
-            $('.selecttable').draggable({
-                connectToSortable:'.sdrop',
-                revert:'.invalid',
-            });
-
+        $('.sdrop').sortable({
+        	
+	});
+            
             $('.sdrop').droppable({
             	greedy: true,
-                accept:'.selecttable , .selectall , .selectcondition , .cloned_selecttable',
+                accept:'.selecttable , .selectall , .selectcondition',
                 drop: function(event, ui) {
-                    if($('#find_selecttable').hasClass('selecttable')){ //オリジナルがドロップした場合のみクローン作成
-                        $(this).append($(ui.draggable).clone());
-                        $('.selecttable').removeClass('selecttable').addClass('cloned_selecttable'); //クローン用にクラス再指定
-                        $('.cloned_selecttable').draggable({
-                        	connectToSortable:'.sdrop',
-                        	revert:'.invalid',
-                        });
-                    }else{
-
-                    }
+                    
+                    $('#find_selecttable').removeClass('cloned_selecttable' + i);
+                    $(function(){
+    			$('#find_selecttable').each(function(){
+        			$(this).addClass('cloned_selecttable' + (i+1));
+        			i++;
+   			});
+		    });
+                    
                 }
             })
         }
@@ -65,12 +64,13 @@ $(function() {
     //円内に配置されたパーツの並び替え
 
 	$('.sspace').sortable({
-		revert: true
+		revert : true
 	});
 
-	$('.sdrop').sortable({
-		revert: true
-	});
+
+//	                $(this).append($(ui.draggable).clone());  クローン作成
+
+
 });
 
 
