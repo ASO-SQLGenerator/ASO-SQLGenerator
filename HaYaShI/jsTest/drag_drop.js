19dab0df31210cf51c95ecd90f5671ff9ac114f0
hayashi <highen4.wtf@gmail.com>
2015-11-11 13:02:54 +0900

    1個できたら1個できんくなるやん

--- HaYaShI/jsTest/drag_drop.js
+++ HaYaShI/jsTest/drag_drop.js
@@ -1,18 +1,18 @@
 	$(function() {
     $('#find_columname').draggable({
-        connectToSortable : '.cdrop',
+        connectToSortable : '.cdrop_sortable',
         helper:'clone',
         revert : 'invalid',
     });
 
         $('#find_num').draggable({
-            connectToSortable : '.cdrop',
+            connectToSortable : '.cdrop_sortable',
             helper:'clone',
             revert : 'invalid',
         });
 
         $('#find_limit').draggable({
-            connectToSortable : '.cdrop',
+            connectToSortable : '.cdrop_sortable',
             helper:'clone',
             revert : 'invalid',
         });
@@ -30,37 +30,40 @@
     });
 
 		$('.cspace').sortable({
-        revert : true,
-        cursor : 'move',
-            stop : function(event,ui) {
+            revert: true,
+            cursor: 'move',
+            stop: function (event, ui) {
 
-                if(!ui.item.hasClass('checked')) {
+
+                if (!ui.item.hasClass('checked')) {
                     ui.item.addClass('checked').attr({name: drop_name, id: "a" + drop_name});
+                    $("*[name=" + drop_name + "] > *[name=culum]").attr('name', "a" + (drop_name + 1));
+
                     drop_name = drop_name + 10;
-                }else{}
+                } else {
+                }
 
+                        $('.cdrop_sortable').sortable({
+                            connectWith: '.cdrop_sortable',
 
-                $('.cdrop').sortable({
-                    connectWith: '.cdrop',
-                    cancel : '.sortDisable',
-                    stop : function(event,ui) {
+                            stop: function (event, ui) {
 
-                        var data1 = $(this).attr('name');
-                        var data = Number(data1);
+                                var data1 = $(this).attr('name');
+                                var data = Number(data1);
 
+                                $("*[name=" + data + "] > div > div > *[name=type]").attr('name', "a" + (data + 2));
+                                $("*[name=" + data + "] > div > *[name=num]").attr('name', "a" + (data + 3));
+                                var limit_name = "a" + (limit_count[data] + data);
+                                $("*[name=" + data + "] > *[name=limit]").attr('name', limit_name);
+                                if ($("*[name=" + limit_name + "]").length > 0) {
+                                    limit_count[data]++;
+                                }
+                            }
+                        })
 
-                        $("*[name=" + data + "] > div > *[name=culum]").attr('name', "a" + (data + 1));
-                        $("*[name=" + data + "] > div > *[name=type]").attr('name', "a" + (data + 2));
-                        $("*[name=" + data + "] > div > *[name=num]").attr('name', "a" + (data + 3));
-                        var limit_name = "a" + (limit_count[data] + data);
-                        $("*[name=" + data + "] > div > *[name=limit]").attr('name', limit_name);
-                        if ($("*[name=" + limit_name + "]").length > 0) {
-                            limit_count[data]++;
-                        }
-                    }
-                })
             }
-        });
+        })
+
 
 
 });
